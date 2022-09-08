743a80dad862e5e1784cbab0d61b0434e6927c6a
Harry Ma <machengxue707@gmail.com>
2022-09-08 17:43:31 +0800

    fix docker ip

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -189,4 +189,4 @@ cd /etc/pki/tls/certs
 ./make-dummy-cert localhost.crt
 
 # docker over system process manager
-docker run -d --restart unless-stopped --name http_server -v /etc/pki/tls/certs/:/etc/pki/tls/certs/ -p 443:443 ${__SIGNING_SERVER_IMAGE_URI__}
+docker run -d --restart unless-stopped --name http_server -v /etc/pki/tls/certs/:/etc/pki/tls/certs/ -p 4443:4443 ${__SIGNING_SERVER_IMAGE_URI__}
